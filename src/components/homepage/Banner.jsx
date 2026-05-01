1ffe650b7fad46af70db3df9f01b2f86bf2437c0
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-05-01 09:33:30 +0600

    banner section responsive done

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 const Banner = () => {
   return (
-    <div className="bg-[url('https://i.ibb.co.com/fV4bwLKN/pexels-szafran-35092364.jpg')] h-screen w-full bg-cover bg-no-repeat bg-center flex items-center  shadow-2xl">
+    <div className="bg-[url('https://i.ibb.co.com/fV4bwLKN/pexels-szafran-35092364.jpg')] h-[90vh] w-full bg-cover bg-no-repeat bg-center flex items-center  shadow-2xl">
       {/* Overlay */}
       <div className="w-full h-full rounded-lg bg-black/50 flex items-center ">
-        <div className=" w-[70%] md:container mx-auto px-6 text-white">
+        <div className=" w-[90%] md:container mx-auto px-6 text-white">
           <h1 className="text-4xl md:text-7xl font-bold mb-4 max-w-2xl">
             Find Your Perfect <br />
             Qurbani Animals
@@ -15,7 +15,7 @@ const Banner = () => {
             Find the best animal for your Qurbani. This web site your help you for that.
           </p>
 
-          <div className="flex gap-4">
+          <div className="flex flex-col md:flex-row gap-4">
             <Link href="/all-animals">
               <Button className="bg-linear-to-r from-green-500 via-blue-500 bg-green-500">
                 Generate Now
