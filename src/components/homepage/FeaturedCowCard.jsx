7b47b4ab6b61af79df1a9aba0e7be73ca67664b0
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-05-01 10:02:59 +0600

    added featured animal card section more style css

--- src/components/homepage/FeaturedCowCard.jsx
+++ src/components/homepage/FeaturedCowCard.jsx
@@ -1,19 +1,27 @@
-import { Card, Chip } from '@heroui/react';
-import Image from 'next/image';
-import React from 'react';
+import { Card, Chip } from "@heroui/react";
+import Image from "next/image";
+import React from "react";
 
-const FeaturedCowCard = ({cow}) => {
-    console.log(cow);
-    return (
-        <Card className='border rounded-lg duration-300 hover:-translate-y-3 cursor-pointer hover:shadow-xl'>
-            <div className="relative w-full h-60 aspect-square rounded-lg overflow-hidden">
-            <Image className='absolute w-full  object-cover rounded-lg' src={cow.image} alt={cow.name} width={300} height={200}/>
-            <Chip className='absolute top-4 right-4' color='accent'>{cow.type}</Chip>
-            </div>
-            <h1 className='text-2xl font-bold'>{cow.name}</h1>
-            <p className='text-lg text-green-500 font-semibold'>{cow.price} TK</p>
-        </Card>
-    );
+const FeaturedCowCard = ({ cow }) => {
+  console.log(cow);
+  return (
+    <Card className="border rounded-lg duration-300 hover:-translate-y-3 cursor-pointer hover:shadow-xl">
+      <div className="relative w-full aspect-square">
+        <Image
+          src={cow.image}
+          alt={cow.name}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className='object-cover rounded-lg'
+        />
+        <Chip className="absolute top-4 right-4" color="accent">
+          {cow.type}
+        </Chip>
+      </div>
+      <h1 className="text-2xl font-bold">{cow.name}</h1>
+      <p className="text-lg text-green-500 font-semibold">{cow.price} TK</p>
+    </Card>
+  );
 };
 
 export default FeaturedCowCard;
