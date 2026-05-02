724bd237d5a06e5bd7c716b34549847eb64119d3
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-05-02 11:44:41 +0600

    update proxy and running in working flow do not give any error

--- src/proxy.js
+++ src/proxy.js
@@ -3,6 +3,7 @@ import { auth } from "./lib/auth";
 import { NextResponse } from "next/server";
 
 
+
 // This function can be marked `async` if using `await` inside
 export async function proxy(request) {
   const session = await auth.api.getSession({
